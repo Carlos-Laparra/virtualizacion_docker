0bd013de1240809f8d10afd6fe11389a13ca87e5
Carlos-Laparra <70443027+Carlos-Laparra@users.noreply.github.com>
2024-01-28 17:02:29 -0600

    Add files via upload

---
+++ Ejercicio 1 - Docker/Conexion_Mysql/Conexion_Mysql/Clases/CConexion.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Conexion_Mysql.Clases
+{
+    class CConexion
+    {
+        MySqlConnection conexion = new MySqlConnection();
+
+        static string servidor = "localhost";
+        static string bd = "db_ejercicio";
+        static string usuario = "root";
+        static string password = "CONTRA";
+        static string puerto = "3304";
+
+        string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
+
+        public MySqlConnection establecerConexion()
+        {
+            try
+            {
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                Console.WriteLine("Se logró conectar a la base de datos correctamente.");
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine("No se pudo realizar la conexión a base de datos, el error es:" + e.ToString());
+            }
+            return conexion;
+        }
+
+        public string Select()
+        {
+
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+
+            MySqlCommand comando = new MySqlCommand("SELECT idnombres as ID, nombrescol as Nombre FROM nombres", conexion);
+            MySqlDataAdapter adaptador = new MySqlDataAdapter();
+            adaptador.SelectCommand = comando;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+
+            string resultados = ConvertirDataTableAString(tabla);
+
+            return resultados;
+        }
+
+
+        public void Insertar(string nombre)
+        {
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            // Preparar la consulta SQL de inserción
+            string consulta = "INSERT INTO `db_ejercicio`.`nombres` (`nombrescol`) VALUES ('" + nombre + "')";
+
+            try { 
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    // Parámetro para evitar SQL injection
+                    comando.Parameters.AddWithValue("@nombre", nombre);
+
+                    // Ejecutar la consulta de inserción
+                    comando.ExecuteNonQuery();
+                }
+
+                Console.WriteLine("Inserción realizada con éxito.");
+            }
+            catch
+            {
+                Console.WriteLine("No se ha realizado la inserción");
+            }
+        }
+
+        public void Actualizar(int id, string nuevoNombre)
+        {
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            // Preparar la consulta SQL de actualización
+            string consulta = "UPDATE `db_ejercicio`.`nombres` SET `nombrescol` = '" + nuevoNombre + "' WHERE (`idnombres` = '" + id + "');";
+
+            try {
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    // Parámetros para evitar SQL injection
+                    comando.Parameters.AddWithValue("@nuevoNombre", nuevoNombre);
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    // Ejecutar la consulta de actualización
+                    comando.ExecuteNonQuery();
+
+                    Console.WriteLine("Actualización realizada con éxito.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("No se ha realizado el update");
+            }
+        }
+
+        public void Borrar(int id)
+        {
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            // Preparar la consulta SQL de actualización
+            string consulta = "DELETE FROM `db_ejercicio`.`nombres` WHERE (`idnombres` = '" + id +"');";
+
+            try
+            {
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexion))
+                {
+                    // Parámetros para evitar SQL injection
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    // Ejecutar la consulta de actualización
+                    comando.ExecuteNonQuery();
+
+                    Console.WriteLine("Delete realizado con éxito.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("No se ha realizado el delete");
+            }
+        }
+
+        static string ConvertirDataTableAString(DataTable tabla)
+        {
+            string resultados = "";
+
+            // Iterar sobre las filas de la tabla
+            foreach (DataRow fila in tabla.Rows)
+            {
+                // Iterar sobre las columnas de cada fila
+                foreach (DataColumn columna in tabla.Columns)
+                {
+                    resultados += $"{columna.ColumnName}: {fila[columna]} | ";
+                }
+
+                resultados += Environment.NewLine; // Nueva línea para la siguiente fila
+            }
+
+            return resultados;
+        }
+
+    }
+}
---
+++ Ejercicio 1 - Docker/Conexion_Mysql/Conexion_Mysql/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion_Mysql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+
+                int opcionElgida = 0;
+
+                while (opcionElgida != 6) {
+                    Clases.CConexion objConexion = new Clases.CConexion();
+                    Console.WriteLine("El menú es el siguiente: " +
+                    "\r\n1. Testear conexión a la base datos" +
+                    "\r\n2. Leer datos" +
+                    "\r\n3. Insertar un dato" +
+                    "\r\n4. Actualizar un dato" +
+                    "\r\n5. Borrar un dato" +
+                    "\r\n6. Salir del programa" +
+                    "\r\n \r\nEleja el número de la opción que desea realizar:");
+
+                    opcionElgida = Convert.ToInt32(Console.ReadLine()); ;
+
+                    /*PARA HACER UNA CONSULTA*/
+                    if (opcionElgida == 1)
+                    {
+                        Console.Clear();
+                        objConexion.establecerConexion();
+                        opcionElgida = Salir();
+                    }
+                    /*PARA HACER UNA CONSULTA*/
+                    else if (opcionElgida == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("- SELECT");
+                        string cadena = objConexion.Select();
+
+
+                        if (cadena == "")
+                        {
+                            Console.WriteLine("No hay datos almacenados.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Estos son los datos actuales:" +
+                                "\r\n" + cadena);
+                        }
+                        Salir();
+                    }
+                    /*PARA HACER UNA INSERCION*/
+
+                    else if (opcionElgida == 3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("- INSERT");
+                        Console.WriteLine("\r\nElija el nombre que desea insertar: ");
+                        string nombre = Console.ReadLine();
+                        objConexion.Insertar(nombre);
+                        opcionElgida = Salir();
+                    }
+
+
+                    else if (opcionElgida == 4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("- UPDATE");
+                        Console.WriteLine("\r\nElija el id que desea actualziar: ");
+                        int idAct = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\r\nElija el nuevo nombre: ");
+                        string nombre = Console.ReadLine();
+                        objConexion.Actualizar(idAct, nombre);
+                        opcionElgida = Salir();
+                    }
+
+                    else if (opcionElgida == 5)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("- DELETE");
+                        Console.WriteLine("\r\nElija el id que desea borrar: ");
+                        int idAct = Convert.ToInt32(Console.ReadLine());
+                        objConexion.Borrar(idAct);
+                        opcionElgida = Salir();
+                    }
+
+                    else if (opcionElgida == 6)
+                    {
+                        Console.WriteLine("Fin del programa, adios :)");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La opción elegida no es válida, ingresar nuevamente.");
+                    }
+                    Console.Clear();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        static int Salir()
+        {
+            Console.WriteLine("Si desea salir del programa presione el número 6, de lo contrario presione cualquier número");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 6)
+            {
+                Console.WriteLine("Fin del programa, adios :)");
+            }
+            return num;
+
+        }
+
+    }
+}
